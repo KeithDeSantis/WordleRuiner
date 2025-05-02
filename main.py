b22a4c66c4e120fbf133b7b244d2af1d91658e34
Keith DeSantis <kwdesantis@wpi.edu>
2025-05-02 18:33:53 -0400

    set up getting victims from hard coded or command line

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import requests
 import re
+import sys
 
 PREVIOUS_WORD = ""
+victims = [("1111111111", "Verizon"), ("222222222", "T-Mobile")]
 
-def getWorldeWord():
+# Set victims if they were passed in
+def getVictims():
+    if(len(sys.argv) < 3):
+       return victims
+    if(len(sys.argv) % 2 == 0):
+        print("Please enter victim phone numbers and providers in the format: \"python wordleRuiner.py XXXXXXXXXX <Provider>\"")
+        exit(1)
+    newVictims = []
+    for i in range(1, len(sys.argv), 2):
+        newVictims.append((sys.argv[i], sys.argv[i+1]))
+    return newVictims
+
+# Get Wordle word from tomsguide.com because pulling it from the NYT JavaScript kinda sucks
+def getWordleWord():
     resp = requests.get('https://www.tomsguide.com/news/what-is-todays-wordle-answer')
     if (resp.ok):
         match = re.search("(?<=Drumroll, please &mdash; it's <strong>)([A-Z]{5})(?=\.<\/strong>)", resp.text)
         if(match):
-            PREVIOUS_WORD = match
             return match.group(1)
     else:
-        return False
+        print("[ERROR]: Failed to find match from tomsguide.com")
+        exit(1)
+
+def sendText():
+    pass
 
 if __name__ == "__main__":
-    print(getWorldeWord())
+    victims = getVictims()
+    print(victims)
