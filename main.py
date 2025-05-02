b56df53f03e78c9c5623589e9ad1d52ca1b74a2a
Keith DeSantis <kwdesantis@wpi.edu>
2025-05-02 19:22:12 -0400

    mwahaha

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import requests
 import re
 import sys
+import threading
+import asyncio
+from email.message import EmailMessage
+from typing import Collection, List, Tuple, Union
+import smtplib
 
-PREVIOUS_WORD = ""
-victims = [("1111111111", "Verizon"), ("222222222", "T-Mobile")]
+with open("email.txt", "r") as f:
+        EMAIL = f.read()
+with open("pass.txt", "r") as f:
+        PASS = f.read()
+HOST = "smtp.gmail.com"
+CARRIERS = {
+    "att": "@mms.att.net",
+    "tmobile": "@tmomail.net",
+    "verizon": "@vtext.com",
+    "sprint": "@messaging.sprintpcs.com"
+}
+
+PREVIOUS_WORD_FILE = "prev.txt"
+victims = [("7819297124", "verizon")]
 
 # Set victims if they were passed in
 def getVictims():
@@ -28,9 +45,44 @@ def getWordleWord():
         print("[ERROR]: Failed to find match from tomsguide.com")
         exit(1)
 
-def sendText():
-    pass
+# Save the Wordle word to prev.txt so we don't send multiple texts in a day
+def saveWord(word):
+    with open(PREVIOUS_WORD_FILE, "w") as f:
+        f.write(word)
+
+# Get the last word we sent
+def getPreviousWord():
+    with open(PREVIOUS_WORD_FILE, "r") as f:
+        return f.read()
+
+# Text the victims the Wordle word
+def sendText(victim_number, victim_carrier, message):
+    victim = victim_number + CARRIERS[victim_carrier]
+    auth = (EMAIL, PASS)
+
+    server = smtplib.SMTP(HOST, 587)
+    server.starttls()
+    server.login(auth[0], auth[1])
+    server.sendmail(auth[0], victim, message)
+
+# This function ruins someone's day
+def ruinWordle():
+    # Get the Wordle word from tomsguide.com
+    word = getWordleWord()
+    # If the word hasn't changed, don't send a new text
+    if(getPreviousWord() == word):
+        return
+    
+    # Get our victim info (either hardcoded or passed in as command line args)
+    victims = getVictims()
+    
+    # Send texts
+    for v in victims:
+        sendText(v[0], v[1], f"{word} is today's Wordle word")
+        print(f"Sent to {v}...")
+
+    # Save the word to our prev.txt file to remind ourselves we've already sent a text for today
+    saveWord(word)
 
 if __name__ == "__main__":
-    victims = getVictims()
-    print(victims)
+    ruinWordle()
